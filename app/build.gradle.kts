eab01b207ca235e607231e3c34bedc4cbf792f7b
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-01-31 15:20:25 +0100

    Update build scripts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 import groovy.lang.Closure
-import rocks.poopjournal.metadataremover.Version
 
 /*
  * MIT License
@@ -34,6 +33,12 @@ plugins {
     id("kotlin-android-extensions")
     // Kotlin annotation processor plugin
     id("kotlin-kapt")
+    // Google Play Store Publishing
+//    id("com.github.triplet.play") version "2.1.0"
+    // F-Droid Publishing
+//    id("org.openintents.fdroid") version "0.2"
+    // JaCoCo test coverage reports
+    id("com.vanniktech.android.junit.jacoco") version "0.13.0"
 }
 
 android {
@@ -58,6 +63,7 @@ android {
         getByName("debug") {
             // Append "DEBUG" to all debug build versions
             versionNameSuffix = " DEBUG"
+            isTestCoverageEnabled = true
         }
 
         // Production builds
@@ -87,21 +93,28 @@ android {
         @Suppress("unchecked_cast")
         all(closure as Closure<Test>)
     }
-
-//    packagingOptions {
-//        exclude("/META-INF/INDEX.LIST")
-//        exclude("/INDEX.LIST")
-//        exclude("/META-INF/DEPENDENCIES")
-//        exclude("/META-INF/DEPENDENCIES.txt")
-//        exclude("/DEPENDENCIES")
-//        exclude("/DEPENDENCIES.txt")
-//        exclude("/META-INF/ASL2.0")
-//        exclude("/META-INF/*.handlers")
-//        exclude("/META-INF/*.schemas")
-//        exclude("/META-INF/**.*")
-//        exclude("/mozilla/*.*")
-//    }
 }
+
+junitJacoco {
+    includeInstrumentationCoverageInMergedReport = true
+}
+
+//play {
+//    val credentialsFile = project
+//            .ext
+//            .properties["googleplay.credentials"]
+//            ?.let(::file)
+//    if (credentialsFile == null) {
+//        System.err.println("Could not find Google Play credentials.")
+//        commit = false
+//    }
+//    else {
+//        serviceAccountCredentials = credentialsFile
+//    }
+//
+//    track = "internal"
+//    resolutionStrategy = "fail"
+//}
 
 dependencies {
     // Kotlin
@@ -111,18 +124,18 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.1.0")
 
     // Material Design
-    implementation("com.google.android.material:material:1.1.0-alpha02")
+    implementation("com.google.android.material:material:1.1.0-alpha03")
 
     // AppCompat support
     implementation("androidx.appcompat:appcompat:1.1.0-alpha01")
-    implementation("androidx.core:core-ktx:1.1.0-alpha03")
+    implementation("androidx.core:core-ktx:1.1.0-alpha04")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.recyclerview:recyclerview:1.1.0-alpha01")
+    implementation("androidx.recyclerview:recyclerview:1.1.0-alpha02")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.exifinterface:exifinterface:1.0.0")
     // Architecture components
-    implementation("androidx.lifecycle:lifecycle-extensions:2.1.0-alpha01")
-    kapt("androidx.lifecycle:lifecycle-compiler:2.1.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.1.0-alpha02")
+    kapt("androidx.lifecycle:lifecycle-compiler:2.1.0-alpha02")
 
     // Timber logging util
     implementation("com.jakewharton.timber:timber:4.7.1")
@@ -136,15 +149,14 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.0")
 
     // Okio I/O
-    implementation("com.squareup.okio:okio:2.1.0")
+    implementation("com.squareup.okio:okio:2.2.1")
     implementation("com.github.bumptech.glide:glide:4.8.0")
     //implementation("org.apache.commons:commons-imaging:1.0-R1401825")
     //implementation("org.apache.tika:tika-core:1.17")
     //implementation("org.apache.tika:tika-parsers:1.17")
     implementation("com.drewnoakes:metadata-extractor:2.11.0")
     // https://mvnrepository.com/artifact/ar.com.hjg/pngj
-    implementation(group = "ar.com.hjg", name = "pngj", version = "2.1.0")
-
+    implementation("ar.com.hjg:pngj:2.1.0")
 
     //implementation("com.getkeepsafe.taptargetview:taptargetview:1.9.1")
     implementation("com.sothree.slidinguppanel:library:3.4.0")
