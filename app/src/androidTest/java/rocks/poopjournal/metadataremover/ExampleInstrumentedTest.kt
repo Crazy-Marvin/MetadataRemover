0ae2db47e8ed7581a5bb117497e0e71354c5258b
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-01-31 15:59:59 +0100

    Fix instrumentation tests

--- app/src/androidTest/java/rocks/poopjournal/metadataremover/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/rocks/poopjournal/metadataremover/ExampleInstrumentedTest.kt
@@ -1,12 +1,11 @@
 package rocks.poopjournal.metadataremover
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,7 +17,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
         assertEquals("rocks.poopjournal.metadataremover", appContext.packageName)
     }
 }
